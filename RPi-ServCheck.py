732e2caf5a8eb95d1ace8ed3471d53e980ce66ea
Martin Collins <martin_p_coll@hotmail.com>
2020-03-13 01:27:08 -0400

    Further work tonight...

--- RPi-ServCheck.py
+++ RPi-ServCheck.py
@@ -3,6 +3,7 @@
 try:
 	import sys
 	import os
+	import subprocess
 	import configparser
 
 	from email.mime.multipart import MIMEMultipart
@@ -64,7 +65,7 @@ def getSettings():
 			sys.exit(1)
 
 		if section == 'General':
-			for option in [ 'Enabled' ]:
+			for option in [ 'Enabled','OKExitCode' ]:
 				if not config.has_option(section, option):
 					print("ERROR: Missing " + section + " option: " + option +". Please check " + settings_filename)
 					sys.exit(1)
@@ -78,7 +79,9 @@ def getSettings():
 	# Settings file sections and options valid. Now retrieve/parse values and store in global dicts
 	try:
 		# Populate General dict
-		GENERAL = {'ENABLED':config.getboolean('General', 'Enabled')}
+		GENERAL = {
+			'ENABLED':config.getboolean('General', 'Enabled'),
+			'OKEXITCODE':config.getint('General','OKExitCode')}
 		# Populate Email Settings dict
 		EMAILSETTINGS =	{
 			'FROM_EMAIL':config.get('EmailSettings', 'FromEmail'),
@@ -93,7 +96,7 @@ def getSettings():
 
 		################## DEBUGGING ##################
 		if (debug):
-			print("DEBUG: Dumping Dictionary Keys & Values:\n")
+			print("DEBUG INFO: Dumping Dictionary Keys & Values:\n")
 			for key, value in GENERAL.items():
 				print(key, value)
 			for key, value in EMAILSETTINGS.items():
@@ -102,7 +105,7 @@ def getSettings():
 				print(key, value)
 			for key, value in SERVICES.items():
 				print(key, value)
-			print("\nDEBUG: Dump Complete")
+			print("\nDEBUG INFO: Dump Complete")
 		###############################################
 
 	except ValueError as e:
@@ -115,17 +118,52 @@ def main():
 	global EXITCODES
 	global EMAILSETTINGS
 
+	noOfOKServices = 0
+	noOfNotOKServices = 0
+	serviceStatusStr = ''
+	failedServicesStr = ''
+
 	# Check Enabled flag in settings file is set to True, otherwise print message and exit
 	if not(GENERAL['ENABLED']):
 		print("INFO: 'Enabled' flag in settings file is not set to True. Exiting...")
 		sys.exit(0)
 
-	# Iterate through Services dict, check Service status for each valid Service
+	# Iterate through Services dict, check Service status for each enabled Service
 	for serviceName,toCheck in SERVICES.items():
+		# Only check enabled services
 		if toCheck.lower() == 'true':
+			# Build OS Command to check Service status
 			osCommand = 'systemctl status ' + serviceName + ' > /dev/null 2>&1'
+			# Execute OS Command and extract Exit Code
 			exitCode = os.WEXITSTATUS(os.system(osCommand))
-			print (serviceName + ' exit code is: ' + str(exitCode) + ' : ' + EXITCODES.get(str(exitCode), 'Unknown Exit Code'))
+			if (debug):
+				print ('DEBUG INFO: ' + serviceName + ' exit code is: ' + str(exitCode) + ' : ' + EXITCODES.get(str(exitCode), 'Unknown Exit Code'))
+			# Based on extracted Exit Code, increment OK and Not OK counts
+			if exitCode == GENERAL['OKEXITCODE']:
+				noOfOKServices += 1
+			else:
+				noOfNotOKServices += 1
+			# Add to string keeping a record of services checked along with their Exit Code info.
+			serviceStatusStr += serviceName + ': ' + str(exitCode) + ' (' + EXITCODES.get(str(exitCode), 'Unknown Exit Code') + ')\n'
+
+	# Now check for any systemd units in failed status
+	osCommand = 'systemctl list-units --state=failed --no-legend'
+	proc = subprocess.Popen(osCommand, stdout=subprocess.PIPE, shell=True)
+	osCommandOutput = proc.stdout.read().decode('UTF-8')
+
+	i = 0
+	if len(osCommandOutput) > 0:
+		failedServicesStr = 'The following services are in a failed state:\n'
+		for line in osCommandOutput.split('\n'):
+			if len(line) > 0:
+				if i > 0:
+					failedServicesStr += '\n'
+				failedServicesStr += line.split(' ')[0]
+				i += 1
+	else:
+		failedServicesStr = 'No systemd units are in a failed state.'
+
+	print("#" + failedServicesStr + "#")
 
 
 if __name__ == '__main__':
@@ -137,11 +175,11 @@ if __name__ == '__main__':
 	# Script is being run as root. Continue...
 	# Debug Status
 	if (debug):
-		print("INFO: DEBUGGING ENABLED\n")
+		print("DEBUG INFO: DEBUGGING ENABLED\n")
 	chkArgs(sys.argv[1:])
 	getSettings()
 	main()
 	# Program complete. Exit cleanly
 	if (debug):
-		print("INFO: Process completed successfully. Exiting...")
+		print("DEBUG INFO: Process completed successfully. Exiting...")
 	sys.exit(0)
