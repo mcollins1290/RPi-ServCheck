cefd9d38a4c41a8a07ab8d3d8c219622be4c0205
Martin Collins <martin_p_coll@hotmail.com>
2021-04-06 16:03:49 -0400

    Pushing changes

--- midnight-launcher.sh
+++ midnight-launcher.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+LOGTOSQL=true
+
 cd "$(dirname "$0")";
 ./RPi-ServCheck.py T > ./Log/midnight-log 2>&1
+retVal=$?
+
+if [ "$LOGTOSQL" = true ] ; then
+	logtext=$(cat ./Log/midnight-log)
+	echo 'Logging to Pi Health Check MariaDB enabled'
+	result=$(curl -G --data-urlencode "code=SERV" \
+			 --data-urlencode "status=${retVal}" \
+			 --data-urlencode "context=D" \
+			 --data-urlencode "comment=${logtext}" \
+			 --data-urlencode "hostname=$HOSTNAME" \
+			http://localhost:5000/insert/checklog)
+
+	echo "Log Result: $result"
+fi
--- reboot-launcher.sh
+++ reboot-launcher.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
+LOGTOSQL=true
+
 cd "$(dirname "$0")";
 sleep 5m
 touch reboot
 ./RPi-ServCheck.py T > ./Log/reboot-log 2>&1
+retVal=$?
+
+if [ "$LOGTOSQL" = true ] ; then
+	logtext=$(cat ./Log/reboot-log)
+	echo 'Logging to Pi Health Check MariaDB enabled'
+	result=$(curl -G --data-urlencode "code=SERV" \
+			 --data-urlencode "status=${retVal}" \
+			 --data-urlencode "context=R" \
+			 --data-urlencode "comment=${logtext}" \
+			 --data-urlencode "hostname=$HOSTNAME" \
+			http://localhost:5000/insert/checklog)
+
+	echo "Log Result: $result"
+fi
